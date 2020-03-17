f5c86e83207bb81da6d7918c709817d326a7ca99
MORENO ROJAS John Jairo <johnmorenoing@gmail.com>
2020-03-17 20:54:21 +0100

    Fix reques body

--- validacion-identidad-web/src/main/java/com/samtel/adapters/primary/rest/validarotp/ValidarOTPController.java
+++ validacion-identidad-web/src/main/java/com/samtel/adapters/primary/rest/validarotp/ValidarOTPController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class ValidarOTPController {
     }
 
     @PostMapping("/")
-    public ResponseEntity<Response> validarOTP(GeneralPayload<OtpInput> otpPayload) {
+    public ResponseEntity<Response> validarOTP(@RequestBody GeneralPayload<OtpInput> otpPayload) {
         Response response = modelMapper
                 .map(validarOTPService.validarOTP(otpConverterPayloadToDto
                         .requestToDto(otpPayload)), Response.class);
--- validacion-identidad-web/src/main/java/com/samtel/adapters/primary/rest/verificarpreguntas/VerificarPreguntasOTPController.java
+++ validacion-identidad-web/src/main/java/com/samtel/adapters/primary/rest/verificarpreguntas/VerificarPreguntasOTPController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class VerificarPreguntasOTPController {
     }
 
     @PostMapping("/")
-    public ResponseEntity<Response> verificarPreguntas(GeneralPayload<VerificarPreguntasInput> verificarPreguntas) {
+    public ResponseEntity<Response> verificarPreguntas(@RequestBody GeneralPayload<VerificarPreguntasInput> verificarPreguntas) {
         Response response = modelMapper.map(verificarPreguntasService
                 .verificarPreguntas(preguntasConverterPayloadToDto.requestToDto(verificarPreguntas)), Response.class);
         return new ResponseEntity<>(response, HttpStatus.OK);
