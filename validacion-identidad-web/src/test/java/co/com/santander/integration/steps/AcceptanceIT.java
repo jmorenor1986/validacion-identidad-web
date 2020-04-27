a9d956262a78b3e2a58dec71c22de316cc35cab6
jmorenor1986 <johnmorenoing@gmail.com>
2020-04-27 17:58:06 +0200

    Integration steps

--- validacion-identidad-web/src/test/java/co/com/santander/integration/steps/AcceptanceIT.java
+++ validacion-identidad-web/src/test/java/co/com/santander/integration/steps/AcceptanceIT.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features/",
-        plugin = {"pretty", "html:target/cucumber/samtel"},
-        glue = "com.samtel.integration.steps")
+        plugin = {"pretty", "html:target/cucumber/santander"},
+        glue = "co.com.santander.integration.steps")
 public class AcceptanceIT {
 }
